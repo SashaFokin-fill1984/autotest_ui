88f6882f1437edf869c537dc24b4fc370450f470
Александр <Fill1984@bk.ru>
2025-07-25 15:36:24 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,18 @@
+def test_user():
+    print("hello world")
+
+
+class TestClass:
+    def test_method(self):
+        print("method")
+
+    def test_method2(self):
+        print("method2")
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, "2 + 2 should be 4"
